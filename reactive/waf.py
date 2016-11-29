50bc496e5eeaeec25cecf1d1dd700b583f91903d
Tilman Baumann <tilman.baumann@canonical.com>
2016-11-29 15:35:04 +0100

    WAF install apache and libapache2-mod-security2 and start apache

--- reactive/waf.py
+++ reactive/waf.py
@@ -1,17 +1,33 @@
 from charms.reactive import when, when_not, set_state
+from charmhelpers.core.hookenv import (
+    open_port,
+    close_port,
+    log,
+    config as orig_config_get,
+    relations_of_type,
+    relation_set,
+    relation_ids,
+    unit_get,
+    status_set
+)
+from charmhelpers.core.host import service_restart, service_start, service_stop
 
 
-@when_not('waf.installed')
-def install_waf():
-    # Do your setup here.
-    #
-    # If your charm has other dependencies before it can install,
-    # add those as @when() clauses above., or as additional @when()
-    # decorated handlers below
-    #
-    # See the following for information about reactive charms:
-    #
-    #  * https://jujucharms.com/docs/devel/developer-getting-started
-    #  * https://github.com/juju-solutions/layer-basic#overview
-    #
-    set_state('waf.installed')
+@when('apache.start')
+@when_not('apache.started')
+def start_apache():
+    status_set('maintenance', 'Starting Apache')
+    assert service_start('apache2'), 'Failed to start Apache2'
+    set_state('apache.started')
+
+
+@when('apache.started')
+def started():
+    status_set('active', 'Ready')
+
+
+@when('apache.available')
+def setup_apache():
+    status_set('maintenance', 'Setting up Apache')
+    # prepare apache stuff
+    set_state('apache.start')
