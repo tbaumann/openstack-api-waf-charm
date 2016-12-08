6bb214e0b406cfe206e088033db34146a8fb66b0
Tilman Baumann <tilman.baumann@canonical.com>
2016-12-08 15:52:26 +0100

    WAF Responsive to relations. Rough version.
    Still a little rough around the edges.
    Most config defaults are wrong.
    Frontend relations are not implemented yet.

--- reactive/waf.py
+++ reactive/waf.py
@@ -1,19 +1,28 @@
-from charms.reactive import when, when_not, set_state, hook, is_state
+import os
+import re
+import subprocess
+import base64
+from functools import reduce
+from charms.reactive import when, when_not, set_state, hook, remove_state
 from charmhelpers.core.hookenv import (
     open_port,
     close_port,
     log,
     config as orig_config_get,
-    relations_of_type,
     relation_set,
-    relation_ids,
     unit_get,
-    status_set
+    status_set,
+    in_relation_hook,
+    relation_types
 )
 from charmhelpers.core.host import service_reload, service_start, service_stop
+from charms.reactive.helpers import data_changed
+from charmhelpers.core.templating import render
+import pprint
 
 
 @when('apache.start')
+@when('waf.available')
 @when_not('apache.started')
 def start_apache():
     status_set('maintenance', 'Starting Apache')
@@ -30,16 +39,149 @@ def started():
 @when_not('apache.started')
 def setup_apache():
     status_set('maintenance', 'Setting up Apache')
-    # prepare apache stuff
+    enable_module('security2')
+    enable_module('proxy')
+    enable_module('rewrite')
+    enable_module('proxy_http')
+    enable_module('proxy_html')
+    # Empty unused ports.conf
+    if os.path.exists('/etc/apache2/ports.conf'):
+        open('/etc/apache2/ports.conf', 'w').write('')
+    status_set('maintenance', 'Waiting for relatioons to join')
     set_state('apache.start')
 
 
+@when('apache.changed')
 @when('apache.available')
-@hook('config-changed')
-def config_changed():
-    config = hookenv.config()
-    log("config-changed():")
-    # Do stuff
-    if is_state('apache.started'):
-        assert service_reload('apache2'), 'Failed to reload Apache'
-    status_set('maintenance', '')
+def reload_apache():
+    log('apache.changed Reloading apache')
+    service_reload('apache2')
+    remove_state('apache.changed')
+
+
+@hook('{requires:http}-relation-{joined,changed}')
+def setup_backend(backend):
+    if not in_relation_hook():
+        return
+    relation_name = backend.relation_name
+    service = relation_name[:relation_name.rindex('-backend')]  # Slice -backend
+    log("Backend {} available".format(service))
+    write_vhost(backend, service)
+    # FIXME open port
+    # Set available once the first relation joined. Simple but works
+    set_state('waf.available')
+
+
+
+@hook('{requires:http}-relation-{departed,broken}')
+def stop_backend(backend):
+    relation_name = backend.relation_name
+    service = relation_name[:relation_name.rindex('-backend')]  # Slice -backend
+    log("Stop backend {}".format(service))
+    try:
+        os.remove('/etc/apache2/sites-enabled/{}.conf'.format(service))
+    except FileNotFoundError:
+        pass
+    set_state('apache.changed')
+
+
+@when('config.changed')
+def write_waf_config():
+    write_file_from_option(
+        '/etc/modsecurity/waf.conf',
+        'securityrules'
+        )
+    write_file_from_option(
+        '/etc/apache2/ssl/webservers-CA.pem',
+        'ssl_webserver_ca'
+        )
+    for service in get_all_servicenames():
+        write_file_from_option(
+            '/etc/apache2/waf/{}.conf'.format(service),
+            service + '_overwrite',
+            remove_if_empty=True
+            )
+        write_file_from_option(
+            '/etc/modsecurity/{}/waf.conf'.format(service),
+            service + '_securityrules',
+            remove_if_empty=True
+            )
+        write_file_from_option(
+            '/etc/apache2/ssl/{}-cert.pem'.format(service),
+            service + '_ssl_cert'
+            )
+        write_file_from_option(
+            '/etc/apache2/ssl/{}-key.pem'.format(service),
+            service + '_ssl_key'
+            )
+
+
+# Helper functions
+def get_all_servicenames():
+    return map(lambda x: x[:x.rindex('-backend')],
+               filter(lambda t: t.endswith('-backend'),
+                      relation_types()
+                     )
+              )
+
+
+def write_file_from_option(path, option_name, remove_if_empty=False):
+    config = orig_config_get()
+    directory = os.path.dirname(path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    if not config[option_name] and remove_if_empty:
+        log("Not writing {} because {} is empty".format(path, option_name))
+        try:
+            os.remove(path)
+        except FileNotFoundError:
+            pass
+        return
+    if not data_changed('waf_{}'.format(option_name), config[option_name]):
+        log("No changes for {}".format(option_name))
+        return
+    log('Writing {}'.format(path))
+    with open(path, 'w') as f:
+        f.write(str(base64.b64decode(config[option_name]), 'utf-8'))
+    set_state('apache.changed')
+
+
+def write_vhost(backend, service):
+    config = extract_service_config(service, orig_config_get())
+    context = {
+        'service': service,
+        'hosts': reduce(
+           lambda x, y: x + y, map(
+                lambda srv: srv['hosts'], backend.services())),  # Flatten hosts in one list
+    }
+    context.update(config)
+    if not data_changed(service + '.vhost', context):
+        log("No changes for {}".format(service))
+        return
+    render(
+        source='proxy_vhost.template',
+        target='/etc/apache2/sites-enabled/{}.conf'.format(service),
+        owner='root',
+        perms=0o644,
+        context=context
+        )
+    set_state('apache.changed')
+
+
+def extract_service_config(service, config):
+    result = {}
+    for key in filter(lambda k: k.startswith(service + '_'), config.keys()):
+        result[re.sub(service + '_', '', key)] = config[key]
+    return result
+
+
+def enable_module(module=None):
+    if module is None:
+        return True
+    if os.path.exists("/etc/apache2/mods-enabled/%s.load" % (module)):
+        log("Module already loaded: %s" % module)
+        return True
+    return_value = subprocess.call(['/usr/sbin/a2enmod', module])
+    if return_value != 0:
+        return False
+    set_state('apache.changed')
